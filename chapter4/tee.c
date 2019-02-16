a10e49b0450404fc76de29a111aec2692d8d50f3
Vladik <qvviko@gmail.com>
2019-02-16 13:29:55 +0300

    before changes

--- chapter4/tee.c
+++ chapter4/tee.c
@@ -1,5 +1,9 @@
 #include "tlpi_hdr.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
+
+    if (argc == 1 || strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+        usageErr("%s [options] filename", argv[0]);
+    }
     return 0;
 }
